aa5d8e45c24970a833403c13260b5c18885b6f27
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-22 05:52:34 +0100

    remove pyright

--- nvim/.config/nvim/lua/appez/lazy/lsp.lua
+++ nvim/.config/nvim/lua/appez/lazy/lsp.lua
@@ -1,30 +1,45 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
 
-    config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        require("lspconfig.health").check()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "angularls",
-                "bashls",
-                "html",
-                "ts_ls",
-                "pyright",
-            },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-            },
-        })
-    end,
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("lspconfig.health").check()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"angularls",
+				"bashls",
+				"html",
+				"ts_ls",
+			},
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				-- ["basepyright"] = function()
+				-- 	require("lspconfig").basepyright.setup({
+				--
+				-- 		settings = {
+				-- 			basepyright = {
+				-- 				analysis = {
+				-- 					inlayHints = {
+				-- 						callArgumentNames = false,
+				-- 						functionReturnTypes = false,
+				-- 						variableTypes = false,
+				-- 					},
+				-- 				},
+				-- 			},
+				-- 		},
+				-- 	})
+				-- end,
+			},
+		})
+	end,
 }
