afae419832f45dc34c109c46039743cc3100e781
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-27 16:54:15 +0100

    add remap for python script

--- nvim/.config/nvim/lua/appez/remap.lua
+++ nvim/.config/nvim/lua/appez/remap.lua
@@ -24,3 +24,6 @@ vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+-- Python
+vim.keymap.set("n", "<leader>py", ":sp<CR>:term python3 %<CR>")
