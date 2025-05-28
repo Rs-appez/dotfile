bf2fa76108518a2fdc4752bfea00587673a36017
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-05-28 23:20:29 +0200

    add django template

--- nvim/.config/nvim/lua/appez/lazy/lsp.lua
+++ nvim/.config/nvim/lua/appez/lazy/lsp.lua
@@ -1,46 +1,39 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
+    "neovim/nvim-lspconfig",
+    dependencies = {
 
-		{ "williamboman/mason.nvim", version = "^1.0.0" },
-		{ "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
-	},
+        { "williamboman/mason.nvim",           version = "^1.0.0" },
+        { "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
+    },
 
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		require("lspconfig.health").check()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"angularls",
-				"bashls",
-				"html",
-				"ts_ls",
-			},
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-				-- ["basepyright"] = function()
-				-- 	require("lspconfig").basepyright.setup({
-				--
-				-- 		settings = {
-				-- 			basepyright = {
-				-- 				analysis = {
-				-- 					inlayHints = {
-				-- 						callArgumentNames = false,
-				-- 						functionReturnTypes = false,
-				-- 						variableTypes = false,
-				-- 					},
-				-- 				},
-				-- 			},
-				-- 		},
-				-- 	})
-				-- end,
-			},
-		})
-	end,
+    config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        require("lspconfig.health").check()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "angularls",
+                "bashls",
+                "html",
+                "ts_ls",
+            },
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+            },
+        })
+        require("lspconfig").djlsp.setup({
+            cmd = { "/home/appez/.local/bin/djlsp" },
+            filetypes = { "htmldjango" },
+            init_options = {
+                django_settings_module = "",
+                docker_compose_file = "docker-compose.yml",
+                docker_compose_service = "django",
+            },
+        })
+    end,
 }
