93619f2bbaaba3663e5f870fa51bbc39d64b698d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-18 09:05:23 +0200

    add markdown render

--- nvim/.config/nvim/lua/appez/lazy/markdow-preview.lua
+++ nvim/.config/nvim/lua/appez/lazy/markdow-preview.lua
@@ -1,19 +1,19 @@
 -- install without yarn or npm
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && yarn install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
-	keys = {
-		{
-			"<leader>mk",
-			function()
-				vim.cmd("MarkdownPreviewToggle")
-			end,
-			desc = "Markdown Preview Start/Stop",
-		},
-	},
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown", "mermaid" }
+    end,
+    ft = { "markdown", "mermaid" },
+    keys = {
+        {
+            "<leader>mk",
+            function()
+                vim.cmd("MarkdownPreviewToggle")
+            end,
+            desc = "Markdown Preview Start/Stop",
+        },
+    },
 }
