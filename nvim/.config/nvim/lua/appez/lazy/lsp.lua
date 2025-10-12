d31df9bcd5dc33b61bce2bae5670b58b88051568
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-12 18:21:58 +0200

    add lazydev

--- nvim/.config/nvim/lua/appez/lazy/lsp.lua
+++ nvim/.config/nvim/lua/appez/lazy/lsp.lua
@@ -2,8 +2,19 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
 
-        { "williamboman/mason.nvim",            },
-        { "williamboman/mason-lspconfig.nvim",  },
+        { "williamboman/mason.nvim", },
+        { "williamboman/mason-lspconfig.nvim", },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
     },
 
     config = function()
