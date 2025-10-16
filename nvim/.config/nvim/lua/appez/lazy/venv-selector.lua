1bee7421e23b15ff22d9b0ad267e8d9df0bd75e4
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-16 20:50:33 +0200

    remove unused dependencies

--- nvim/.config/nvim/lua/appez/lazy/venv-selector.lua
+++ nvim/.config/nvim/lua/appez/lazy/venv-selector.lua
@@ -2,9 +2,8 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = {
         "neovim/nvim-lspconfig",
-        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-    lazy = false,
+    ft = "python",
     keys = {
         { ",v", "<cmd>VenvSelect<cr>" },
     },
