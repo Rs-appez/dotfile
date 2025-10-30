9b4007e7d843dd1d080a5d75a632f354a99b7ea6
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-30 12:06:08 +0100

    comment wip remap

--- nvim/.config/nvim/lua/appez/remap.lua
+++ nvim/.config/nvim/lua/appez/remap.lua
@@ -19,8 +19,8 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "<leader>rr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/g<Left><Left>]], { desc = "Replace word under cursor" })
-vim.keymap.set("v", "<leader>rr", [[:s/\%V<C-r><C-w>/<C-r><C-w>/g<Left><Left>]],
-    { desc = "Replace word under cursor in visual selection" })
+-- vim.keymap.set("v", "<leader>rr", [[:s/\%V<C-r><C-w>/<C-r><C-w>/g<Left><Left>]],
+--     { desc = "Replace word under cursor in visual selection" })
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
