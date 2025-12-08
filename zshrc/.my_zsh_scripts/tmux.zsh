d41592bb9a2f903e34271c7d4cdc7a073cc8b17a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-12-08 03:44:07 +0100

    feat(zsh): add tnt function for tmux session management
    Introduce the `tnt` function to create or attach to a tmux session named
    after the current directory (excluding leading dot), or a user-provided
    name. This streamlines tmux workflow by reducing manual session naming.

--- zshrc/.my_zsh_scripts/tmux.zsh
+++ zshrc/.my_zsh_scripts/tmux.zsh
@@ -21,3 +21,19 @@ tn(){
     tmux attach -t "$SESSION_NAME"
 
 }
+
+tnt(){
+    if [ -z "$1" ]; then
+        name=$(basename "$PWD")
+        if [[ "${name:0:1}" == "." ]]; then
+            name=${name:1}
+        fi
+        SESSION_NAME=$name
+        echo "$SESSION_NAME"
+
+    else
+        SESSION_NAME="$1"
+    fi
+
+    tmux new -A -s "$SESSION_NAME"
+}
