95cbd08b55a6c64524b24dc5a55c308309620e9d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-04-05 10:23:24 +0200

    refactor(nvim): move remap.lua to config/keymaps.lua
    Renamed remap.lua to config/keymaps.lua for better organization of keymap
    configurations. Updated require statements in init.lua to reflect the new
    location. This change improves the structure and clarity of the Neovim
    configuration files.

--- nvim/.config/nvim/lua/appez/init.lua
+++ nvim/.config/nvim/lua/appez/init.lua
@@ -1,6 +1,5 @@
-require("appez.remap")
+require("appez.config.keymaps")
 require("appez.config.options")
 require("appez.config.autocmds")
 require("appez.mapping_lang")
 require("appez.config.lazy")
-
