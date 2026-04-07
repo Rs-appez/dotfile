a2e44a7d7cb13c2cd82f06a8e4332596e91198e1
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-04-07 07:33:56 +0200

    fix(snacks): align keymap formatting and fix lsp reference key
    Refactor keymap table for consistent alignment and whitespace. Change
    the LSP references keymap from "gr" to "gR" to avoid conflicts and
    improve discoverability. No functional changes except for the updated
    LSP references mapping.

--- nvim/.config/nvim/lua/appez/plugins/snacks.lua
+++ nvim/.config/nvim/lua/appez/plugins/snacks.lua
@@ -5,7 +5,7 @@ return {
     ---@type snacks.Config
     opts = {
         bigfile = { enabled = true },
-        indent = { enabled = true, animate = {enabled = false} },
+        indent = { enabled = true, animate = { enabled = false } },
         input = { enabled = true },
         notifier = {
             enabled = true,
@@ -44,15 +44,15 @@ return {
         { "<leader>gS",      function() Snacks.picker.git_stash() end,                               desc = "Git Stash" },
         { "<leader>gd",      function() Snacks.picker.git_diff() end,                                desc = "Git Diff (Hunks)" },
         -- github
-        { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
-        { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
-        { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
-        { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+        { "<leader>gi",      function() Snacks.picker.gh_issue() end,                                desc = "GitHub Issues (open)" },
+        { "<leader>gI",      function() Snacks.picker.gh_issue({ state = "all" }) end,               desc = "GitHub Issues (all)" },
+        { "<leader>gp",      function() Snacks.picker.gh_pr() end,                                   desc = "GitHub Pull Requests (open)" },
+        { "<leader>gP",      function() Snacks.picker.gh_pr({ state = "all" }) end,                  desc = "GitHub Pull Requests (all)" },
         -- Grep
         { "<leader>/",       function() Snacks.picker.grep() end,                                    desc = "Grep" },
         { "<leader>sb",      function() Snacks.picker.lines() end,                                   desc = "Buffer Lines" },
         { "<leader>sB",      function() Snacks.picker.grep_buffers() end,                            desc = "Grep Open Buffers" },
-        { "<leader>sw",      function() Snacks.picker.grep_word() end,                               desc = "Visual selection or word", mode = { "n", "x", "v" } },
+        { "<leader>sw",      function() Snacks.picker.grep_word() end,                               desc = "Visual selection or word",   mode = { "n", "x", "v" } },
         -- search
         { '<leader>s"',      function() Snacks.picker.registers() end,                               desc = "Registers" },
         { '<leader>s/',      function() Snacks.picker.search_history() end,                          desc = "Search History" },
@@ -74,7 +74,7 @@ return {
         -- LSP
         { "gd",              function() Snacks.picker.lsp_definitions() end,                         desc = "Goto Definition" },
         { "gD",              function() Snacks.picker.lsp_declarations() end,                        desc = "Goto Declaration" },
-        { "gr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
+        { "gR",              function() Snacks.picker.lsp_references() end,                          nowait = true,                       desc = "References" },
         { "gI",              function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
         { "gy",              function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
         { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
@@ -85,12 +85,12 @@ return {
         { "<leader>n",       function() Snacks.notifier.show_history() end,                          desc = "Notification History" },
         { "<leader>bd",      function() Snacks.bufdelete() end,                                      desc = "Delete Buffer" },
         { "<leader>cR",      function() Snacks.rename.rename_file() end,                             desc = "Rename File" },
-        { "<leader>gB",      function() Snacks.gitbrowse() end,                                      desc = "Git Browse",               mode = { "n", "v" } },
+        { "<leader>gB",      function() Snacks.gitbrowse() end,                                      desc = "Git Browse",                 mode = { "n", "v" } },
         { "<leader>gg",      function() Snacks.lazygit() end,                                        desc = "Lazygit" },
         { "<leader>un",      function() Snacks.notifier.hide() end,                                  desc = "Dismiss All Notifications" },
         { "<c-_>",           function() Snacks.terminal() end,                                       desc = "Toggle Terminal" },
-        { "]]",              function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference",           mode = { "n", "t" } },
-        { "[[",              function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference",           mode = { "n", "t" } },
+        { "]]",              function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference",             mode = { "n", "t" } },
+        { "[[",              function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference",             mode = { "n", "t" } },
         {
             "<leader>N",
             desc = "Neovim News",
