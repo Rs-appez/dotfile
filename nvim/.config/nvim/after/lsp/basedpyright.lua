a4aacdaced1570cff5a03376246f24d643543c8a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-04-08 13:17:00 +0200

    chore(lsp): relax basedpyright diagnostics config
    Disable several diagnostic warnings in basedpyright, including implicit
    relative imports and unknown type reports for variables, members,
    parameters, and lambdas. This reduces noise and focuses on more relevant
    issues during Python development.

--- nvim/.config/nvim/after/lsp/basedpyright.lua
+++ nvim/.config/nvim/after/lsp/basedpyright.lua
@@ -1,11 +1,16 @@
 return {
-    settings = {
-        basedpyright = {
-            analysis = {
-                diagnosticSeverityOverrides = {
-                    reportUnannotatedClassAttribute = false,
-                },
-            },
-        },
-    },
+	settings = {
+		basedpyright = {
+			analysis = {
+				diagnosticSeverityOverrides = {
+					reportUnannotatedClassAttribute = false,
+					reportImplicitRelativeImport = false,
+					reportUnknownVariableType = false,
+					reportUnknownMemberType = false,
+					reportUnknownParameterType = false,
+					reportUnknownLambdaType = false,
+				},
+			},
+		},
+	},
 }
