4e5afaf2908a44912f31076a157820fb9e85eaf1
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-23 08:08:56 +0200

    remove nvim config from this repo

--- nvim/.config/nvim/chatWithCopilot.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-        require("CopilotChat").open()
-        vim.cmd("wincmd j")
-        vim.cmd("q")
-    end,
-})
--- nvim/.config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-pcall(function()
-	local env = require("appez.utils.env")
-	-- Config-level secrets
-	env.load(vim.fn.stdpath("config") .. "/.env")
-	-- Project-level overrides
-	env.load(vim.fn.getcwd() .. "/.env")
-end)
-
-require("appez")
